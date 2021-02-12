ea519a0b15f4cc0a9d1805930b78f8f567b202c0
Nacho Gallo <nacho.r.gallo@gmail.com>
2021-02-11 21:19:26 -0300

    Se refactoriza insta

--- src/components/RoomVolume.js
+++ src/components/RoomVolume.js
@@ -1,114 +1,114 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useRef } from "react";
 import { useForm } from "react-hook-form";
+import {
+  Container,
+  Button,
+  Input,
+  Select,
+  Stack,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from "@chakra-ui/react";
+import { render } from "@testing-library/react";
 
 const RoomVolume = () => {
-  const { register, errors, handleSubmit } = useForm();
+  const [room, setRoom] = useState({
+    volumen: null,
+    alto: null,
+    ancho: null,
+    largo: null,
+    cadr: null,
+    cambioAire: null,
+    ventilacion: null,
+  });
+  const [isLoading, setIsLoading] = useState(false);
 
-  const [Altura, setAltura] = useState();
-  const [Ancho, setAncho] = useState();
-  const [Largo, setLargo] = useState();
-  const [Volumen, setVolumen] = useState();
-  const [CambiosAire, setCambiosAire] = useState();
-  // const [Ventilacion, setVentilacion] = useState();
-  const [Cadr, setCadr] = useState();
+  const inputRef = useRef([]);
 
-  const onSubmit = (e) => {
-    setVolumen(Ancho * Largo * Altura);
-    const resultado = Volumen * (CambiosAire - 4);
-    setCadr(resultado);
-    console.log(Cadr);
+  const calcular = (e) => {
+    setIsLoading(true);
+    const { alto, ancho, largo, ventilacion, cambioAire } = room;
+    const result = parseInt(alto * ancho * largo * (cambioAire - ventilacion));
+    setRoom({ ...room, ["cadr"]: result });
+    setIsLoading(false);
   };
 
   return (
-    <Fragment>
-      <h1>Formulario</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          name="ancho"
-          onChange={(e) => {
-            const opcion = e.target.value;
-            setAncho(opcion);
-          }}
-          className="form-control my-2"
-          type="number"
-          ref={register({
-            required: { value: true, message: "Ancho obligatorio" },
-          })}
-        />
-        <span className="text-danger text-small d-block mb-2">
-          {errors?.ancho?.message}
-        </span>
-        <input
-          name="largo"
-          onChange={(e) => {
-            const opcion = e.target.value;
-            setLargo(opcion);
-          }}
-          className="form-control my-2"
-          type="number"
-          ref={register({
-            required: { value: true, message: "Largo obligatorio" },
-          })}
-        />
-        <span className="text-danger text-small d-block mb-2">
-          {errors?.largo?.message}
-        </span>
-        <input
-          name="alto"
-          onChange={(e) => {
-            const opcion = e.target.value;
-            setAltura(opcion);
-          }}
-          className="form-control my-2"
-          type="number"
-          ref={register({
-            required: { value: true, message: "Alto obligatorio" },
-          })}
-        />
-        <span className="text-danger text-small d-block mb-2">
-          {errors?.alto?.message}
-        </span>
-        <input
-          name="cambiosAire"
-          onChange={(e) => {
-            const opcion = e.target.value;
-            setCambiosAire(opcion);
-          }}
-          className="form-control my-2"
-          type="number"
-          ref={register({
-            required: {
-              value: true,
-              message: "Cantidad de cambios de aire obligatoria",
-            },
-          })}
-        />
-        <span className="text-danger text-small d-block mb-2">
-          {errors?.cambiosAire?.message}
-        </span>
-
-        {/*         <select
-          name="ventilacion"
-          class="form-select"
-          aria-label="Default select example"
-          onChange={(e) => {
-            const opcion = e.target.value;
-            setVentilacion(opcion);
-          }}
-        >
-          <option selected>Ventilación del recinto</option>
-          <option value="0.5">Mala ventilación</option>
-          <option value="1">Ventilación baja</option>
-          <option value="1.5">Normal de colegio</option>
-          <option value="3">Buena ventilación</option>
-          <option value="4">Ventilación mejorada con sistema</option>
-        </select> */}
-        <button type="submit" className="btn btn-primary my-3">
-          Calcular
-        </button>
-      </form>
-      <p>CADR: {Ancho * Altura * Largo * (CambiosAire - 4)}m3/h</p>
-    </Fragment>
+    <>
+      <Container>
+        <form action="">
+          <Stack spacing={5}>
+            <Input
+              type="number"
+              name="alto"
+              placeholder="Altura"
+              onChange={() =>
+                setRoom({ ...room, ["alto"]: inputRef.current[0].value })
+              }
+              size="md"
+              ref={(el) => (inputRef.current[0] = el)}
+            />
+            <Input
+              type="number"
+              name="ancho"
+              placeholder="Ancho"
+              size="md"
+              ref={(el) => (inputRef.current[1] = el)}
+              onChange={() =>
+                setRoom({ ...room, ["ancho"]: inputRef.current[1].value })
+              }
+            />
+            <Input
+              type="number"
+              name="largo"
+              placeholder="Largo"
+              size="md"
+              ref={(el) => (inputRef.current[2] = el)}
+              onChange={() =>
+                setRoom({ ...room, ["largo"]: inputRef.current[2].value })
+              }
+            />
+            <Input
+              type="number"
+              name="cambioAire"
+              placeholder="Cambios de aire"
+              size="md"
+              ref={(el) => (inputRef.current[3] = el)}
+              onChange={() =>
+                setRoom({ ...room, ["cambioAire"]: inputRef.current[3].value })
+              }
+            />
+            <Select
+              ref={(el) => (inputRef.current[4] = el)}
+              placeholder="Seleciona una opcion"
+              value={room.ventilacion}
+              onChange={(el) =>
+                setRoom({ ...room, ["ventilacion"]: inputRef.current[4].value })
+              }
+            >
+              <option value="0.5">0.5</option>
+              <option value="1">1</option>
+              <option value="1.5">1.5</option>
+            </Select>
+            <Button
+              onClick={() => calcular()}
+              colorScheme="green"
+              isLoading={isLoading}
+              loadingText="Calculando..."
+            >
+              Calcular
+            </Button>
+            {room?.cadr && <p>Cadr: {room.cadr} m3/h</p>}
+          </Stack>
+        </form>
+      </Container>
+    </>
   );
 };
 
